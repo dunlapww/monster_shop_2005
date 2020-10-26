af9b20b981a15e2bda309d0a6e26235d21f59eb4
Will Dunlap <dunlapww@gmail.com>
2020-10-26 17:40:53 -0600

    Add feature test for nav bar links

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -33,5 +33,56 @@ RSpec.describe 'Site Navigation' do
       end
 
     end
+    # User Story 2, Visitor Navigation
+  
+    # As a visitor
+    # I see a navigation bar
+    # This navigation bar includes links for the following:
+    # - a link to return to the welcome / home page of the application ("/")
+    # - a link to browse all items for sale ("/items")
+    # - a link to see all merchants ("/merchants")
+    # - a link to my shopping cart ("/cart")
+    # - a link to log in ("/login")
+    # - a link to the user registration page ("/register")
+  
+    # Next to the shopping cart link I see a count of the items in my cart
+
+    describe 'As a vistor' do
+      describe 'I see a navigation bar with several links including:' do
+
+        it "a link to return to the welcome / home page of the application ('/')" do
+          visit '/merchants'
+          within 'nav' do
+            click_link("Welcome")
+            expect(current_path).to eq("/")
+          end
+        end
+
+        it "a link to browse all items for sale ('/items')" do
+          visit '/merchants'
+          within 'nav' do
+            click_link("Items")
+            expect(current_path).to eq("/items")
+          end
+        end
+        
+        it "a link to my shopping cart ('/cart')" do
+          visit '/merchants'
+          within 'nav' do
+            click_link("Cart")
+            expect(current_path).to eq("/cart")
+          end
+        end
+
+        it "a link to log in ('/login')" do
+          visit '/merchants'
+          within 'nav' do
+            click_link("Login")
+            expect(current_path).to eq("/login")
+          end
+        end
+
+      end
+    end
   end
 end
