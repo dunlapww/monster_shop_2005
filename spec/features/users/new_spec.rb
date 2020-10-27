8749d81365f33d73e7948a063da35581c340475f
Connor Ferguson <cpfergus1@gmail.com>
2020-10-26 23:42:10 -0300

    Updated story 12 test

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -59,35 +59,36 @@ describe "as a visitor" do
     end
   end
 
-  #User Story 12, Registration Email must be unique
+#User Story 12, Registration Email must be unique
   describe "When I visit the user registration page" do
     describe "If I fill out the registration form" do
       describe "But include an email address already in the system" do
         describe "Then I am returned to the registration page" do
           describe "My details are not saved and I am not logged in" do
             describe "The form is filled in with all previous data except the email field and password fields" do
-              it "I see a flash message telling me the email address is already in use"
-              current_user = User.create!({name: "Joe",
-                                           address: "123 Joe St.",
-                                           city: "Joesville",
-                                           state: "KY",
-                                           zip: "81620",
-                                           email_address: "joe@example.com",
-                                           password: "Joeisthebest!",
-                                           password_confirmation: "Joeisthebest!"})
+              it "I see a flash message telling me the email address is already in use" do
+                current_user = User.create!({name: "James",
+                address: "123 James St.",
+                city: "Jamessville",
+                state: "KY",
+                zip: "81620",
+                email_address: "james@example.com",
+                password: "Jamesisthebest!",
+                password_confirmation: "Jamesisthebest!"})
 
-              visit('/register')
-              fill_in :name, with: "Jim"
-              fill_in :address, with: "123 Jim St."
-              fill_in :city, with: "Jimsville"
-              fill_in :state, with: "KY"
-              fill_in :zip, with: "81620"
-              fill_in :email_address, with: "joe@example.com"
-              fill_in :password, with: "Jimisthebest!"
-              fill_in :password_confirmation, with: "Jimisthebest!"
+                visit('/register')
+                fill_in :name, with: "Jim"
+                fill_in :address, with: "123 Jim St."
+                fill_in :city, with: "Jimsville"
+                fill_in :state, with: "KY"
+                fill_in :zip, with: "81620"
+                fill_in :email_address, with: "james@example.com"
+                fill_in :password, with: "Jimisthebest!"
+                fill_in :password_confirmation, with: "Jimisthebest!"
 
-              click_on("Submit")
-              expect(page).to have_content("E-mail already taken, please input another!") 
+                click_on("Submit")
+                expect(page).to have_content("E-mail already taken, please input another!")
+              end
             end
           end
         end
