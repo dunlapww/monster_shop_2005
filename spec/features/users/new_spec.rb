a7d63cf36cecf9af647dd801d075836cebfaad44
Connor Ferguson <cpfergus1@gmail.com>
2020-10-26 22:53:35 -0300

    Implemented test for story 11

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -26,4 +26,34 @@ describe "as a visitor" do
       expect(page).to have_content("Thank you for logging in Joe")
     end
   end
+  describe "When I visit the user registration page" do
+    describe "And I do not fill in this form completely," do
+      it "I am returned to the registration page and told i did something wrong" do
+        visit('/register')
+        fill_in :name, with: "Joe"
+        fill_in :address, with: "123 Joe St."
+        fill_in :city, with: "Joesville"
+        fill_in :state, with: "KY"
+        fill_in :zip, with: "81620"
+        fill_in :password, with: "Joeisthebest!"
+        fill_in :password_confirmation, with: "Joeisthebest!"
+        click_on("Submit")
+        expect(page).to have_content("User not created, missing required field inputs")
+        expect(current_path).to eq("/register")
+      end
+      it "I am returned to the registration page and told i did something wrong" do
+        visit('/register')
+        fill_in :name, with: "Joe"
+        fill_in :address, with: "123 Joe St."
+        fill_in :city, with: "Joesville"
+        fill_in :state, with: "KY"
+        fill_in :zip, with: "81620"
+        fill_in :email_address, with: "joe@example.com"
+        fill_in :password, with: "Joeisthebest!"
+        click_on("Submit")
+        expect(page).to have_content("User not created, missing required field inputs")
+        expect(current_path).to eq("/register")
+      end
+    end
+  end
 end
