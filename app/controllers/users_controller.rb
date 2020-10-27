0896257a2f566bcddda76e265dab139262a034ea
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-26 18:48:23 -0600

    Create feature test for new user and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def new
-    
+
+  end
+
+  def create
+    user = User.create(user_params)
+    flash[:notice] = "Thank you for logging in #{user.name}"
+
+    redirect_to "/profile"
+  end
+
+  private
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email_address, :password)
   end
 end
