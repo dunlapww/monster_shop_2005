9672bfcab17ac4600c2cabbc34e03a6b76328373
Will Dunlap <dunlapww@gmail.com>
2020-10-27 18:45:30 -0600

    Add logic to create session action for merchant

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     if user.authenticate(params[:password])
       session[:user_id]=user.id
       flash[:success] = "Thank you for logging in #{user.name}"
-      redirect_to '/profile'
+      redirect_to '/profile' if user.role == "default"
+      redirect_to '/merchant' if user.role == "merchant_employee"
     else
       flash.now[:error] = "Invalid credentials, please try again"
       render :new
