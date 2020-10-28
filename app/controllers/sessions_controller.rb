e30bb173a7825d34f0676e3fe292bf802b238404
Will Dunlap <dunlapww@gmail.com>
2020-10-27 19:00:50 -0600

    Add functionality to sessions create action for admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       flash[:success] = "Thank you for logging in #{user.name}"
       redirect_to '/profile' if user.role == "default"
       redirect_to '/merchant' if user.role == "merchant_employee"
+      redirect_to '/admin' if user.role == "admin"
     else
       flash.now[:error] = "Invalid credentials, please try again"
       render :new
