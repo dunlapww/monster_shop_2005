e0bbb261d251041904d29d765c5151e5463e278b
Eugene Theriault <hybridbassist@gmail.com>
2020-10-30 08:34:33 -0700

    Edit indentation and spacing in user show spec

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,18 +1,20 @@
 #User Story 19, User Profile Show Page
 require 'rails_helper'
+
 describe 'As a registered user' do
   before(:each) do
-  @user = User.create!({password: "userpass",
-                         name: "username",
-                         address: "useraddress",
-                         city: "usercity",
-                         state: "userstate",
-                         zip: "userzip",
-                         email_address: "useremail",
-                         password_confirmation: "userpass",
-                         role: 0})
+    @user = User.create!({password: "userpass",
+                          name: "username",
+                          address: "useraddress",
+                          city: "usercity",
+                          state: "userstate",
+                          zip: "userzip",
+                          email_address: "useremail",
+                          password_confirmation: "userpass",
+                          role: 0})
     allow_any_instance_of(ActionDispatch::Request).to receive(:session){{user_id: @user.id}}
   end
+
   describe 'When I visit my profile page' do
     it 'Then I see all of my profile data on the page except my password' do
       visit 'profile'
@@ -24,6 +26,7 @@ describe 'As a registered user' do
       expect(page).to have_content("#{@user.email_address}")
       expect(page).to_not have_content("#{@user.password}")
     end
+
     it 'And I see a link to edit my profile data' do
       visit 'profile'
       expect(page).to have_link("Edit Profile")
