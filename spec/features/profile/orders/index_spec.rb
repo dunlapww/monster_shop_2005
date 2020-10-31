986420b44c5937d6fa4d8e438c8bb54e49245545
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-31 12:52:32 -0600

    Create feature test for 28 and update index view for profile orders

--- spec/features/profile/orders/index_spec.rb
+++ spec/features/profile/orders/index_spec.rb
@@ -8,16 +8,23 @@ feature 'user show page' do
       create_list(:merchant, 2)
       @order1 = create(:order, user_id: user.id)
       @order2 = create(:order, user_id: user.id)
-      @order1.items << create_list(:item, 5)
-      @order2.items << create_list(:item, 5)
+      create_list(:item_order, 5, order_id: @order1.id)
+      create_list(:item_order, 5, order_id: @order2.id)
     end
 
     describe 'I see a list of all my orders including many details:' do
       it 'order ID as link, creation date, order updated, status, quantity of items, grand_total of items' do
         visit "/profile/orders"
-        
+
+        within("#order-#{@order1.id}") do
+          expect(page).to have_link("order-#{@order1.id}")
+          expect(page).to have_content("#{@order1.created_at.strftime("%m-%d-%Y")}")
+          expect(page).to have_content("#{@order1.updated_at.strftime("%m-%d-%Y")}")
+          expect(page).to have_content("#{@order1.status}")
+          expect(page).to have_content("#{@order1.quantity_of_items}")
+          expect(page).to have_content("#{@order1.grandtotal}")
+        end
       end
     end
   end
 end
-
