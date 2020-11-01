3adc2f7044fcbff840999e6aca68eccea8451b90
Will Dunlap <dunlapww@gmail.com>
2020-11-01 14:50:20 -0700

    Add tests for user story 35

--- spec/features/merchant/dashboard/show_spec.rb
+++ spec/features/merchant/dashboard/show_spec.rb
@@ -11,6 +11,19 @@ feature 'As a merchant' do
       @merchant1 = create(:merchant)
       @merchant1.users << @merchant_employee
       page.set_rack_session(user_id: @merchant_employee.id)
+      @customer1 = create(:user)
+      @customer2 = create(:user)
+      @order1 = create(:order, user_id: @customer1.id)
+      @order2 = create(:order, user_id: @customer2.id, status: "shipped")
+      @order3 = create(:order, user_id: @customer2.id)
+      @item1 = create(:item, merchant_id: @merchant1.id)
+      @item2 = create(:item, merchant_id: @merchant1.id)
+      @item3 = create(:item, merchant_id: @merchant1.id)
+      create(:item_order, order_id: @order1.id, item_id: @item1.id)
+      create(:item_order, order_id: @order1.id, item_id: @item3.id)
+      create(:item_order, order_id: @order2.id, item_id: @item2.id)
+      create(:item_order, order_id: @order3.id, item_id: @item1.id)
+      
     end
     it "I see the name an full address of the merchant I work for" do
       visit "/merchant"
@@ -19,6 +32,16 @@ feature 'As a merchant' do
       expect(page).to have_content(@merchant_employee.merchant.city)
       expect(page).to have_content(@merchant_employee.merchant.state)
       expect(page).to have_content(@merchant_employee.merchant.zip)
+    end
+    it "I see any pending orders that contain items of mine and their details" do
+      visit "/merchant"
+      within "#order-#{@order1.id}" do
+        expect(page).to have_link(@order1.id)
+        expect(page).to have_content(@order1.created_at.to_date)
+        expect(page).to have_content(@order1.merchant_items_qty(@merchant1.id))
+        expect(page).to have_content(@order1.merchant_items_value(@merchant1.id))
+      end
+      expect(page).to_not have_selector("#order-#{@order2.id}")
     end
   end
 end
