076091bbd1149d6c9a26e2f5d1e68013bf2dff4f
Connor Ferguson <cpfergus1@gmail.com>
2020-11-02 11:50:27 -0300

    implemented user-story 42-43 functionality

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,4 +2,13 @@ class Merchant::ItemsController < ApplicationController
   def index
     @items = User.find(session[:user_id]).merchant.items
   end
+
+  def update
+    item = Item.find(params[:item_id])
+    if params[:change_active?]
+      item.toggle!(:active?)
+      flash[:sucess] = item.active? ? "#{item.name} is now available for sale" : "#{item.name} is no longer available for sale"
+      redirect_to '/merchant/items'
+    end
+  end
 end
