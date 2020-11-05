adb3003f2d843542b45051aaaf117219949a5cf2
Eugene Theriault <hybridbassist@gmail.com>
2020-11-05 14:49:52 -0800

    Remove redundant require_admin method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,4 @@ class AdminController < ApplicationController
   def show
     @orders = Order.status_sort
   end
-
-  private
-
-  def require_admin
-    render file: "/public/404" unless current_admin?
-  end
 end
