926d6ada7093e5c87bff83d3883b10fdb55fcfca
Will Dunlap <dunlapww@gmail.com>
2020-11-21 13:05:29 -0700

    update routes through line 59

--- config/routes.rb
+++ config/routes.rb
@@ -3,38 +3,60 @@ Rails.application.routes.draw do
 
   root "merchants#index"
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
-  # This is going to change
+  # get "/merchants", to: "merchants#index"
+  # get "/merchants/new", to: "merchants#new"
+  # get "/merchants/:id", to: "merchants#show"
+  # post "/merchants", to: "merchants#create"
+  # get "/merchants/:id/edit", to: "merchants#edit"
+  # patch "/merchants/:id", to: "merchants#update"
+  # delete "/merchants/:id", to: "merchants#destroy"
+  resources :merchants
+  
+  #below can't be converted to resources syntax...
   get '/merchant', to: 'merchant/dashboard#show'
-  #patch '/merchant/items', to: 'merchant/items#update'
-  namespace :merchant do
-    resources :items, except: [:show]
-    resources :orders, only: :show
-  end
+  
 
-  resources :item_orders, only: :update
+  # namespace :merchant do
+  #   #resources :items, except: [:show]
+  # end
+  get '/merchant/items', to: 'merchant/items#index'
+  get '/merchant/items/new', to: 'merchant/items#new', as: 'new_merchant_item'
+  post '/merchant/items', to: 'merchant/items#create'
+  get '/merchant/items/:id/edit', to: 'merchant/items#edit', as: 'edit_merchant_item'
+  patch '/merchant/items/:id', to: 'merchant/items#update', as: 'merchant_item'
+  delete '/merchant/items/:id', to: 'merchant/items#destroy'
+  
+  # namespace :merchant do
+  #   resources :orders, only: :show
+  # end
+  get '/merchant/orders/:id', to: 'merchant/orders#show', as: 'merchant_order'
 
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
+  #resources :item_orders, only: :update
+  patch "/item_orders/:id", to: 'item_orders#update'
+
+
+  resources :items, only: [:index, :show, :edit, :update]
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
+
+  #below can't be converted resources syntax...
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  
+  resources :items, only: :destroy
+  #delete "/items/:id", to: "items#destroy"
 
+  #below can't be converted to resources syntax...
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
 
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :reviews, only: [:edit, :update, :destroy]
+  # get "/reviews/:id/edit", to: "reviews#edit"
+  # patch "/reviews/:id", to: "reviews#update"
+  # delete "/reviews/:id", to: "reviews#destroy"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
