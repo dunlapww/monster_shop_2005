264fb2656f280d83bab0803f4369a654e91883a2
Will Dunlap <dunlapww@gmail.com>
2020-11-21 13:50:08 -0700

    complete route refactor

--- config/routes.rb
+++ config/routes.rb
@@ -58,46 +58,80 @@ Rails.application.routes.draw do
   # patch "/reviews/:id", to: "reviews#update"
   # delete "/reviews/:id", to: "reviews#destroy"
 
+  #below can't be converted to resources syntax...
   post "/cart/:item_id", to: "cart#add_item"
-  get "/cart", to: "cart#show"
-  delete "/cart", to: "cart#empty"
   delete "/cart/:item_id", to: "cart#remove_item"
   patch '/cart/:item_id/add', to: "cart#increment_item"
   patch '/cart/:item_id/subtract', to: 'cart#decrement_item'
+  delete "/cart", to: "cart#empty"
+  get "/cart", to: "cart#show"
+  
+  # get "/orders/new", to: "orders#new"
+  # post "/orders", to: "orders#create"
+  # get "/orders/:id", to: "orders#show"
+  resources :orders, only: [:new, :create, :show]
 
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
-  get "/orders/:id", to: "orders#show"
-
+  #below can't be converted to resoures syntax...
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
-  #users
+  
+  # post "/users", to: "users#create"
+  resources :users, only: :create
+  
+  #below can't be converted to resources syntax...
+  patch "/users", to: "users#update"
   get "/register", to: "users#new"
-  post "/users", to: "users#create"
   get "/profile", to: "users#show"
   get "/profile/edit", to: "users#edit"
-  patch "/users", to: "users#update"
 
-  namespace :profile do
-    resources :orders, only: :index
+  # namespace :profile do
+  #   resources :orders, only: :index
+  # end
+  get "profile/orders", to: 'profile/orders#index'
+
+  # get "/profile/orders/:id", to: "orders#show"
+  # patch '/profile/orders/:id', to: "orders#update"
+  scope :profile do
+    resources :orders, only: [:show, :update]
   end
-  get "/profile/orders/:id", to: "orders#show"
-  patch '/profile/orders/:id', to: "orders#update"
-  #password
+  
+  #below can't be converted to resources syntax...
   get '/profile/edit_password', to: 'passwords#edit'
   patch '/passwords', to: 'passwords#update'
 
-  namespace :admin do
-    resources :orders, only: :update
-    resources :merchants, only: [:show, :index, :update]
-    resources :users, only: [:index, :show] do
-      resources :orders, only: :index
-    end
-    resources :merchants, only: :show do
-      resources :items, only: [:index, :new]
-    end
-  end
+  # namespace :admin do
+  #   resources :orders, only: :update
+  # end
+  patch '/admin/orders/:id', to: 'admin/orders#update'
 
+  # namespace :admin do
+  #   resources :merchants, only: [:show, :index, :update]
+  # end
+  get '/admin/merchants/:id', to: 'admin/merchants#show'
+  patch '/admin/merchants/:id', to: 'admin/merchants#update'
+  get '/admin/merchants', to: 'admin/merchants#index'
+
+  
+  # namespace :admin do
+  #   resources :users, only: [:index, :show] do
+  #     resources :orders, only: :index
+  #   end
+  # end
+  get '/admin/users', to: 'admin/users#index', as: 'admin_users'
+  get '/admin/users/:id', to: 'admin/users#show', as: 'admin_user'
+  get '/admin/users/:user_id/orders', to: 'admin/orders#index', as: 'admin_user_orders'
+  
+
+  # namespace :admin do
+  #   resources :merchants, only: :show do
+  #     resources :items, only: [:index, :new]
+  #   end
+  # end
+  get '/admin/merchants/:id', to: 'admin/merchants#show', as: 'admin_merchant'
+  get '/admin/merchants/:merchant_id/items', to: 'admin/items#index', as: 'admin_merchant_items'
+  get '/admin/merchants/:merchant_id/items/new', to: 'admin/items#new', as: 'new_admin_merchant_item'
+
+  #below can't be converted to resources syntax... 
   get '/admin', to: 'admin#show'
 end
